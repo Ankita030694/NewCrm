c8154e6536ed00bdc03f9cff95ca6d870f33068e
Bhavy <bhavyajain817@gmail.com>
2026-01-22 12:02:14 +0530

    changes

--- src/firebase/ama_app.d.ts
+++ src/firebase/ama_app.d.ts
@@ -2,10 +2,12 @@ declare module '@/firebase/ama_app' {
     import { FirebaseApp } from "firebase/app";
     import { Analytics } from "firebase/analytics";
     import { Firestore } from "firebase/firestore";
-    
+    import { FirebaseStorage } from "firebase/storage";
+
     export const app: FirebaseApp;
     export const analytics: Analytics | null;
     export const db: Firestore;
+    export const storage: FirebaseStorage;
 }
 
 
